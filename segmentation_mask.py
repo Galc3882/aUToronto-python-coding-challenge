6a124733e74de6676b695ffdc5a55fa5379ec2b6
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2022-07-20 17:21:59 -0400

    Update segmentation_mask.py

--- segmentation_mask.py
+++ segmentation_mask.py
@@ -3,89 +3,147 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-
 def getImages():
-    """
-    Get images from the input folder
-    """
-    for img in os.listdir(r"./input"):
-        yield cv2.cvtColor(cv2.imread(r"./input/"+img), cv2.COLOR_BGR2RGB)
+        """
+        Get images from the input folder
+        """
+        for img in os.listdir(r"./input"):
+            yield cv2.cvtColor(cv2.imread(r"./input/"+img), cv2.COLOR_BGR2HSV)
+
+class SegmentationMask:
+    def __init__(self):
+        self.orange = np.array([200, 20, 30])  # Orange color IN RGB
+        self.PIC_REDUCTION_FACTOR = 1/8  # reduce the size of the image to speed up the process
+        self.COLOR_THRESHOLD = 500  # threshold for the distance between colors
+        self.PIXEL_THRESHOLD =  8 # threshold for the number of pixels that are close to the given color
+
+    def preprocess(self, img):
+        """
+        Preprocess the image by changing the color space to RGB and reshaping the image to 3 arrays
+        """
+        return np.float32(img.reshape((-1, 3)))
 
 
-def preprocess(img):
-    """
-    Preprocess the image by changing the color space to RGB and reshaping the image to 3 arrays
-    """
-    return np.float32(img.reshape((-1, 3)))
+    def labelInialCentroids(self, img):
+        """
+        Label the initial centroids with the closest colors to the given color
+        """
+        # # creat a numpy array with the same size as the image and set all the values to 0
+        # labels = np.zeros(
+        #     len(img)*len(img[0]), dtype="uint8").reshape(len(img), len(img[0]))
+        # # loop through the image and put 255 in the labels array if the color is close to the given color
+        # # else increase the threshold and try again
+        # selection = 0 
+        # colorThreshold = 0
+        # while selection < self.PIXEL_THRESHOLD:
+        #     colorThreshold += self.COLOR_THRESHOLD
+        #     for i in range(len(img)):
+        #         for j in range(len(img[0])):
+        #             if self.isOrange(img[i][j]):
+        #                 labels[i][j] = 255
+        #                 selection += 1
+        # return labels
+
+        
+        # define range of red color in HSV
+        lower_red = np.array([160,50,50])
+        upper_red = np.array([180,255,255])
+            
+        # Threshold the HSV image using inRange function to get only red colors
+        mask = cv2.inRange(img, lower_red, upper_red)
+        plt.imshow(mask)
 
 
-def labelInialCentroids(color, img):
-    """
-    Label the initial centroids with the closest colors to the given color
-    """
-    # creat a numpy array with the same size as the image and set all the values to 0
-    labels = np.zeros(len(img), dtype="uint8")
-    # loop through the image and put 255 in the labels array if the color is close to the given color
-    for i in range(len(img)):
-        t = distanceBetweenColors(color, img[i])
-        if t < 5000:
-            labels[i] = 255
-    return labels
+    def isOrange(self, color):
+        """
+        calculate the distance between two colors
+        """
+        return (color[0]>345 or color[0] <60) and (color[1]!=0) and (color[1]>10)
 
 
-def distanceBetweenColors(color1, color2):
-    """
-    calculate the distance between two colors
-    """
-    return (color1[0] - color2[0])**2 + (color1[1] - color2[1])**2 + (color1[2] - color2[2])**2
+    def ShowImage(self, reducedImg, labels):
+        """
+        show the images
+        """
+        reducedImg = cv2.cvtColor(reducedImg, cv2.COLOR_HSV2RGB)
+
+        labels = labels.reshape(len(reducedImg), len(reducedImg[0]))
+        labels = [[[0, 0, 0] if labels[i][j] == 0 else reducedImg[i][j] if labels[i][j] == 255 else [0, 0, 100]
+                for j in range(len(labels[0]))] for i in range(len(labels))]
+
+        fig, axs = plt.subplots(1, 2)
+        axs[0].imshow(labels)
+        axs[0].axis("off")
+        axs[1].imshow(reducedImg)
+        axs[1].axis("off")
+
+        # plt.savefig("test.png", bbox_inches='tight')
+        plt.show()
 
 
-def ShowImage(reducedImg, labels):
-    labels = labels.reshape(len(reducedImg), len(reducedImg[0]))
-    labels = [[[0, 0, 0] if labels[i][j] == 0 else reducedImg[i][j]
-               for j in range(len(labels[0]))] for i in range(len(labels))]
-
-    fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(labels)
-    axs[0].axis("off")
-    axs[1].imshow(reducedImg)
-    axs[1].axis("off")
-
-    # plt.savefig("test.png", bbox_inches='tight')
-    plt.show()
+    def findLargestCluster(self, labels):
+        """
+        find the index of the largest cluster of pixels 
+        by finding the longest row and column of pixels that equal 255
+        """
+        sumRows = np.sum(labels, axis=1)
+        i = np.argmax(sumRows)
+        sumColums = np.sum(labels, axis=0)
+        j = np.argmax(sumColums)
+        return i, j
 
 
-def kMeans(img, maxIterations, centroid):
-    """
-    Perform k-means clustering on the image
-    """
+    def indexOfBarrel(self, img):
+        """
+        Find the index of the barrel in the image
+        """
+        # reduce the size of the image to speed up the process
+        reducedImg = cv2.resize(img, dsize=(
+            int(len(img[0])*self.PIC_REDUCTION_FACTOR), int(len(img)*self.PIC_REDUCTION_FACTOR)), interpolation=cv2.INTER_CUBIC)
+        # !adjust the color to the closest color from the image to the given color
+        # !color = self.findMostSimilarColor(reducedImg)
+        # label the image with the closest color
+        label = self.labelInialCentroids(reducedImg)
+        # find the indecies of the centroid of the largest cluster
+        i, j = self.findLargestCluster(label)
 
-    # reduce the size of the image to speed up the process
-    reducedImg = cv2.resize(img, dsize=(
-        int(len(img[0])/16), int(len(img)/16)), interpolation=cv2.INTER_CUBIC)
-    # preprocess the reduced image
-    reducedPredImg = preprocess(reducedImg)
-    # process the actual image for kmeans
-    predImg = preprocess(img)
-    # label the image with the closest color
-    labels = labelInialCentroids(centroid, reducedPredImg)
-    # !show images
-    ShowImage(reducedImg, labels)
-    # preform k-means clustering
-    ret, newLabel, center = cv2.kmeans(
-        predImg, 2, bestLabels=labels, criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, maxIterations, 255.0), attempts=10, flags=cv2.KMEANS_USE_INITIAL_LABELS, centers=np.array([centroid, [0, 0, 0]]))
-    # !show the new image
-    ShowImage(img, newLabel)
-    return newLabel
 
+        label[i][j] = 1
+
+        # !show images
+        self.ShowImage(reducedImg, label)
+        return label
+        
+    def findMostSimilarColor(self, img):
+        """
+        find the most similar color to the given color by averaging the some of the similar colors in the image
+
+        loop through the image and add the colors to the array
+        if the color is close to the given color
+        then return the average of the colors
+        else increase the threshold and try again
+        """
+        colorThreshold = 0
+        while True:
+            colorThreshold += self.COLOR_THRESHOLD
+            print(colorThreshold)
+            selection = []
+            for i in range(len(img)):
+                for j in range(len(img[0])):
+                    t = self.distanceBetweenColors(img[i][j], self.orange)
+                    if t < colorThreshold:
+                        selection.append(img[i][j].tolist())
+            if len(selection) > self.PIXEL_THRESHOLD:
+                closestColor = np.mean(selection, axis=0).astype(int)
+                break
+        return closestColor
 
 def main():
-    Orange = np.array([220, 50, 50])  # Orange color IN RGB
+    seg = SegmentationMask()
     for img in getImages():
-        labels = kMeans(img, 1, Orange)
-        labels = labels.reshape((img.shape[0], img.shape[1]))
-        plt.imshow(labels, cmap="gray")
-        plt.show()
+        labels = seg.indexOfBarrel(img)
+        # plt.imshow(labels, cmap="gray")
+        # plt.show()
 
 
 if __name__ == "__main__":
